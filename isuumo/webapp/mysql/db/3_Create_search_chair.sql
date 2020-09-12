3e2ab82fb07756078bc892184ff99144455c8730
matsu7874 <mtsmtkmt@gmail.com>
2020-09-12 18:48:31 +0900

    add color_idx, kind_idx

--- isuumo/webapp/mysql/db/3_Create_search_chair.sql
+++ isuumo/webapp/mysql/db/3_Create_search_chair.sql
@@ -3,9 +3,7 @@ DROP TABLE IF EXISTS isuumo._search_chair;
 CREATE TABLE isuumo._search_chair AS
     SELECT
         id,
-        color,
         features,
-        kind,
         popularity,
         stock,
         case
@@ -33,7 +31,27 @@ CREATE TABLE isuumo._search_chair AS
             when depth < 110 then 1
             when depth < 150 then 2
             else 3
-        end as d_idx
+        end as d_idx,
+        CASE
+            WHEN color = '黒' THEN 0
+            WHEN color = '白' THEN 1
+            WHEN color = '赤' THEN 2
+            WHEN color = '青' THEN 3
+            WHEN color = '緑' THEN 4
+            WHEN color = '黄' THEN 5
+            WHEN color = '紫' THEN 6
+            WHEN color = 'ピンク' THEN 7
+            WHEN color = 'オレンジ' THEN 8
+            WHEN color = '水色' THEN 9
+            WHEN color = 'ネイビー' THEN 10
+            WHEN color = 'ベージュ' THEN 11
+        END AS color_idx,
+        CASE
+            WHEN color = 'ゲーミングチェア' THEN 0
+            WHEN color = '座椅子' THEN 1
+            WHEN color = 'エルゴノミクス' THEN 2
+            WHEN color = 'ハンモック' THEN 3
+        END AS kind_idx
     FROM
         chair
     WHERE
@@ -45,3 +63,5 @@ CREATE INDEX index_search_chair_price_idx ON isuumo._search_chair(price_idx);
 CREATE INDEX index_search_chair_h_idx ON isuumo._search_chair(h_idx);
 CREATE INDEX index_search_chair_w_idx ON isuumo._search_chair(w_idx);
 CREATE INDEX index_search_chair_d_idx ON isuumo._search_chair(d_idx);
+CREATE INDEX index_search_chair_color_idx ON isuumo._search_chair(color_idx);
+CREATE INDEX index_search_chair_kind_idx ON isuumo._search_chair(kind_idx);
