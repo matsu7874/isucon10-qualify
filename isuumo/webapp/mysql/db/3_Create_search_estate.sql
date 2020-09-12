9680423281cf5a756e89a8c42699d89e91c88a09
nanae <desperado.kazune@gmail.com>
2020-09-12 17:09:37 +0900

    _search_estateテーブルを作成するsqlを追加

---
+++ isuumo/webapp/mysql/db/3_Create_search_estate.sql
@@ -0,0 +1,36 @@
+CREATE TABLE isuumo._search_estate AS
+    SELECT
+        id,
+        latitude,
+        longitude,
+        rent,
+        door_height,
+        door_width,
+        features,
+        popularity,
+        case
+            when rent < 50000 then 0
+            when rent < 100000 then 1
+            when rent < 150000 then 2
+            else 3
+        end case as rent_idx,
+        case
+            when door_height < 80 then 0
+            when door_height < 110 then 1
+            when door_height < 150 then 2
+            else 3
+        end case as dh_idx,
+        case
+            when door_width < 80 then 0
+            when door_width < 110 then 1
+            when door_width < 150 then 2
+            else 3
+        end case as dw_idx
+    FROM
+        estate
+;
+
+ALTER TABLE isuumo._search_estate ADD PRIMARY KEY id;
+CREATE INDEX index_rent_idx ON isuumo._search_estate(rent_idx);
+CREATE INDEX index_dh_idx ON isuumo._search_estate(dh_idx);
+CREATE INDEX index_dw_idx ON isuumo._search_estate(dw_idx);
