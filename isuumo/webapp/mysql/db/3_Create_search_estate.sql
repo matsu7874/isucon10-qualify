460e749d8be9d63c3bb0199c21d177e81fcfaefc
matsu7874 <mtsmtkmt@gmail.com>
2020-09-12 17:18:39 +0900

    fix sql syntax error

--- isuumo/webapp/mysql/db/3_Create_search_estate.sql
+++ isuumo/webapp/mysql/db/3_Create_search_estate.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS isuumo._search_estate;
+
 CREATE TABLE isuumo._search_estate AS
     SELECT
         id,
@@ -13,24 +15,24 @@ CREATE TABLE isuumo._search_estate AS
             when rent < 100000 then 1
             when rent < 150000 then 2
             else 3
-        end case as rent_idx,
+        end as rent_idx,
         case
             when door_height < 80 then 0
             when door_height < 110 then 1
             when door_height < 150 then 2
             else 3
-        end case as dh_idx,
+        end as dh_idx,
         case
             when door_width < 80 then 0
             when door_width < 110 then 1
             when door_width < 150 then 2
             else 3
-        end case as dw_idx
+        end as dw_idx
     FROM
         estate
 ;
 
-ALTER TABLE isuumo._search_estate ADD PRIMARY KEY id;
+ALTER TABLE isuumo._search_estate ADD PRIMARY KEY (id);
 CREATE INDEX index_rent_idx ON isuumo._search_estate(rent_idx);
 CREATE INDEX index_dh_idx ON isuumo._search_estate(dh_idx);
 CREATE INDEX index_dw_idx ON isuumo._search_estate(dw_idx);
